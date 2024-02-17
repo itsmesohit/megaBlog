80d056f25c2fdf63d03e272978775066356663a6
Coderprince660 <kushwahaprince660@gmail.com>
2024-02-17 13:09:04 +0530

    updated UI

---
+++ src/components/PostCard.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import appwriteService from "../appwrite/config";
+import parse from "html-react-parser";
+import { Link } from "react-router-dom";
+
+function PostCard({ $id, title, featuredImage, content }) {
+  const tempDiv = document.createElement("div");
+  tempDiv.innerHTML = content;
+
+  // Extract text content from the temporary div
+  let postPreview = tempDiv.textContent || tempDiv.innerText;
+
+  // Truncate the text to the first 30 characters
+  postPreview = postPreview.substring(0, 100);
+  return (
+    <Link>
+      {/* <div className='w-full bg-gray-100 rounded-xl p-4 h-68'>
+            <div className='w-full mb-4'>
+                <img src={appwriteService.getFilePreview(featuredImage)} alt={title}
+                className='rounded-xl h-48' />
+
+            </div>
+            <h2
+            className='text-xl font-bold'
+            >{title}</h2>
+        </div> */}
+
+      <div className="w-[320px] rounded-md border bg-gray-100 p-4">
+        <img
+          src={appwriteService.getFilePreview(featuredImage)}
+          alt="Laptop"
+          className="h-[200px] p-2 w-full rounded-md object-cover"
+        />
+        <div className="p-4">
+          <h1 className="text-lg font-semibold">{title}</h1>
+          <p className="mt-3 text-sm text-gray-600">
+          {postPreview}
+          </p>
+          <Link to={`/post/${$id}`}>
+            <button
+              type="button"
+              className="mt-4 rounded-sm bg-black px-2.5 py-1 text-[10px] font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+            >
+              Read
+            </button>
+          </Link>
+        </div>
+      </div>
+    </Link>
+  );
+}
+
+export default PostCard;
