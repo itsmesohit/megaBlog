e4fe0f9cead225234a75979e019f3d410f1171f8
Coderprince660 <kushwahaprince660@gmail.com>
2024-02-17 15:18:35 +0530

    Updated UI

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,73 +1,74 @@
-import React from 'react'
-import {Container, Logo, LogoutBtn} from '../index'
-import { Link } from 'react-router-dom'
-import {useSelector} from 'react-redux'
-import { useNavigate } from 'react-router-dom'
+import React from 'react';
+import { Container, Logo, LogoutBtn } from '../index';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 function Header() {
-  const authStatus = useSelector((state) => state.auth.status)
-  const navigate = useNavigate()
+  const authStatus = useSelector((state) => state.auth.status);
+  const navigate = useNavigate();
 
   const navItems = [
     {
       name: 'Home',
-      slug: "/",
-      active: true
-    }, 
+      slug: '/',
+      active: true,
+    },
+    // {
+    //   name: 'Login',
+    //   slug: '/login',
+    //   active: !authStatus,
+    // },
+    // {
+    //   name: 'Signup',
+    //   slug: '/signup',
+    //   active: !authStatus,
+    // },
     {
-      name: "Login",
-      slug: "/login",
-      active: !authStatus,
-  },
-  {
-      name: "Signup",
-      slug: "/signup",
-      active: !authStatus,
-  },
-  {
-      name: "All Posts",
-      slug: "/all-posts",
+      name: 'All Posts',
+      slug: '/all-posts',
       active: authStatus,
-  },
-  {
-      name: "Add Post",
-      slug: "/add-post",
-      active: authStatus,
-  },
-  ]
-
+    },
+    // {
+    //   name: 'Add Post',
+    //   slug: '/add-post',
+    //   active: authStatus,
+    // },
+  ];
 
   return (
     <header className='py-3 shadow bg-gray-500'>
       <Container>
-        <nav className='flex'>
+        <nav className='flex flex-wrap items-center justify-between'>
           <div className='mr-4'>
             <Link to='/'>
-              <Logo width='70px'   />
-
-              </Link>
+              <Logo width='70px' />
+            </Link>
           </div>
-          <ul className='flex ml-auto'>
-            {navItems.map((item) => 
-            item.active ? (
-              <li key={item.name}>
-                <button
-                onClick={() => navigate(item.slug)}
-                className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
-                >{item.name}</button>
-              </li>
-            ) : null
+          <ul className='flex flex-wrap items-center'>
+            {navItems.map(
+              (item) =>
+                item.active && (
+                  <li key={item.name} className='mr-2 mb-2'>
+                    <button
+                      onClick={() => navigate(item.slug)}
+                      className='px-4 py-2 duration-200 hover:bg-blue-100 rounded-full'
+                    >
+                      {item.name}
+                    </button>
+                  </li>
+                )
             )}
             {authStatus && (
               <li>
-                <LogoutBtn />
+                {/* <LogoutBtn /> */}
               </li>
             )}
           </ul>
         </nav>
-        </Container>
+      </Container>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
